809c24ea00a6c0f926f90f1e282fed469122b022
richardo1459 <richardo1459@gmail.com>
2018-10-16 15:55:38 +0700

    Change code.

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,27 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    #return "OpenShift Hello World!"
-    return "Created by Richardo"
-    #return "Friday, October 5th, 2018"
+    kid = raw_input('KID : ')
+	db = raw_input('Rekening Pengirim : ')
+	cr = raw_input('Rekening Penerima : ')
+	amount = raw_input('Jumlah : ')
+
+	url1 = 'http://baronang.com/test/test1.php'
+	data = {'kid': kid, 'db': db, 'cr': cr, 'amount': amount}
+	res = requests.get(url1, params=data, stream=True)
+
+	data = json.loads(res.text)
+	db_name = data['db_name']
+	db_bal = float(data['db_bal'])
+	cr_name = data['cr_name']
+	cr_bal = float(data['cr_bal'])
+	amt = float(amount)
+
+	print 'Nama Pengirim : ', db_name[0]
+	print 'Saldo : Rp. ', '{0:,.2f}'.format(db_bal)
+	print 'Nama Penerima : ', cr_name[0]
+	print 'Saldo : Rp. ', '{0:,.2f}'.format(cr_bal)
+	print 'Jumlah Transfer : Rp. ', '{0:,.2f}'.format(amt)
 
 if __name__ == "__main__":
     application.run()
